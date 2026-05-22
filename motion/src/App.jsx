5453deea57e7de05445db411384df84d3444c9ee
Raj Aryan <raj.aryan2518@gmail.com>
2026-05-22 10:45:16 +0530

    configured tailwindCSS

---
+++ motion/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div  className='bg-red-400'>
+        <h1 className='text-white text-3xl'>Hello World!!</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
